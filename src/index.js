5254e7efe0ab1626228a85f7f029bc8cb59e1286
Nate Koike <nkoike1998@gmail.com>
2021-01-03 01:34:14 -1000

    hello world now showing up using react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
 
-const e = <h1> hello world </h1>;
-
-ReactDOM.render(e, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
