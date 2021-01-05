f1957480527e398b5eae7e1dd3b8d9f0a775ff2b
Nate Koike <nkoike1998@gmail.com>
2021-01-04 22:08:10 -1000

    checking for images with matching text

--- server/deliver.js
+++ server/deliver.js
@@ -6,13 +6,35 @@ function deliver(pageState) {
   // store the filenames that will be loaded for each page
   const fnames = ['en.txt', 'jp.txt'];
 
-  // load text data from the appropriate file
+  const filePath = path.join(
+    __dirname,
+    './src' + pageState.path + '/');
+
+  // all the data to send back
+  let data = { imgs: [] };
+
   try {
-    const data = fs.readFileSync(
-      path.join(__dirname, './src' + pageState.path + '/' + fnames[pageState.selectedLang]),
+    // try to load text data from the appropriate file
+    data.text = fs.readFileSync(
+      path.join(filePath, fnames[pageState.selectedLang]),
       'utf8');
 
-    return data;
+    // check for more data
+    try {
+      // if there is an image subdirectory
+      if (fs.existsSync(path.join(filePath, "/img"))) {
+        // get all the filenames
+        fs.readdirSync(path.join(filePath, "/img")).forEach(file => {
+          // if the file is a .jpg
+          console.log(file);
+        });
+      }
+    }
+    catch(e) {
+      console.log("An error occurred.");
+    }
+
+    return data.text;
   }
   catch (err) {
     console.log(err);
